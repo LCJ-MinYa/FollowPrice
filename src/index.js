050c690b362b31e1e96a218978899c912daa8211
LCJ-MinYa <1049468118@qq.com>
2017-10-30 18:39:28 +0800

    淘宝和天猫价格爬取成功

--- src/index.js
+++ src/index.js
@@ -2,22 +2,38 @@
 
 var schedule = require('node-schedule');
 var request = require("request");
-var cheerio = require("cheerio");
 
 // schedule.scheduleJob('1 * * * * *', function() {
 // 	console.log('测试打印输出', new Date());
 // });
 
 //请求商品价格
-var req = function(url, id) {
-	request(url, function(error, response, body) {
-		if (!error && response.statusCode == 200) {
-			var $ = cheerio.load(body);
-			console.log($('.J-p-' + id).text());
-		} else {
-			console.log('请求失败, error是' + error);
-		}
-	});
-}
+var req = function(url) {
+	// request(url, function(error, response, body) {
+	// 	if (!error && response.statusCode == 200) {
+	// 		console.log(JSON.parse(body)[0].op);
+	// 	} else {
+	// 		console.log('请求失败, error是' + error);
+	// 	}
+	// });
 
-req('https://item.jd.com/16004933996.html', '16004933996');
+	var options = {
+		url: 'https://mdskip.taobao.com/core/initItemDetail.htm?itemId=541799568460',
+		headers: {
+			'referer': 'https://detail.tmall.com/item.htm?id=541799568460'
+		}
+	}
+	request(options, function(error, response, body) {
+		var data = JSON.parse(body);
+		var priceInfo = data.defaultModel.itemPriceResultDO.priceInfo;
+		for (var i in priceInfo) {
+			if (priceInfo[i].promotionList && priceInfo[i].promotionList.length != 0) {
+				console.log(priceInfo[i].promotionList[0].price);
+			} else {
+				console.log(priceInfo[i].price);
+			}
+		}
+	})
+}
+req();
+// req('http://p.3.cn/prices/mgets?type=1&skuIds=1951426058');
