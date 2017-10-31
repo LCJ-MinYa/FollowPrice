ff944fc1352e7ba416c531a807294d87fb5f9602
LCJ-MinYa <1049468118@qq.com>
2017-10-31 11:48:03 +0800

    优化

--- src/index.js
+++ src/index.js
@@ -2,45 +2,15 @@
 
 /*--定时任务--*/
 var Schedule = require('node-schedule');
-/*--请求方法--*/
-var request = require("request");
+/*--获取价格--*/
+var Price = require("./method/price.js");
 /*--发送邮件--*/
 var Mail = require('./method/mail.js');
 
+Price.getProductPrice();
 
 // Schedule.scheduleJob('1 * * * * *', function() {
 // 	console.log('测试打印输出', new Date());
 // });
-
-//请求商品价格
-var req = function(url) {
-	// request(url, function(error, response, body) {
-	// 	if (!error && response.statusCode == 200) {
-	// 		console.log(JSON.parse(body)[0].op);
-	// 	} else {
-	// 		console.log('请求失败, error是' + error);
-	// 	}
-	// });
-
-	var options = {
-		url: 'https://mdskip.taobao.com/core/initItemDetail.htm?itemId=541799568460',
-		headers: {
-			'referer': 'https://detail.tmall.com/item.htm?id=541799568460'
-		}
-	}
-	request(options, function(error, response, body) {
-		var data = JSON.parse(body);
-		var priceInfo = data.defaultModel.itemPriceResultDO.priceInfo;
-		for (var i in priceInfo) {
-			if (priceInfo[i].promotionList && priceInfo[i].promotionList.length != 0) {
-				console.log(priceInfo[i].promotionList[0].price);
-			} else {
-				console.log(priceInfo[i].price);
-			}
-		}
-	})
-}
-
-// req('http://p.3.cn/prices/mgets?type=1&skuIds=1951426058');
 
 // Mail.sendMail('测试发邮件', '<p>Hello world!</p>');
--- src/method/price.js
+++ src/method/price.js
@@ -15,7 +15,6 @@ var getJDProductId = function(str) {
 	return content[0];
 }
 
-
 /**
  * [getProductPrice 获取商品价格]
  * @param {String} url：商品的url链接
@@ -25,27 +24,27 @@ var getJDProductId = function(str) {
 var getProductPrice = function() {
 	for (var i = 0; i < Config.PRICE.length; i++) {
 		//JD商品
-		if (Config.PRICE.url.indexOf('jd.com') > -1) {
-			var productId = getJDProductId(Config.PRICE.url);
-			request(Config.JD + productId, function(error, response, body) {
+		if (Config.PRICE[i].url.indexOf('jd.com') > -1) {
+			var productId = getJDProductId(Config.PRICE[i].url);
+			Request(Config.JD + productId, function(error, response, body) {
 				if (!error && response.statusCode == 200) {
 					console.log(JSON.parse(body)[0].p);
 				} else {
-					console.log('请求JD商品' + Config.PRICE.name + '失败', error);
+					console.log('请求JD商品' + Config.PRICE[i].name + '失败', error);
 				}
 			});
 			continue;
 		}
 
 		//TMALL商品
-		if (Config.PRICE.url.indexOf('tmall.com') > -1) {
+		if (Config.PRICE[i].url.indexOf('tmall.com') > -1) {
 			var options = {
 				url: 'https://mdskip.taobao.com/core/initItemDetail.htm?itemId=541799568460',
 				headers: {
 					'referer': 'https://detail.tmall.com/item.htm?id=541799568460'
 				}
 			}
-			request(options, function(error, response, body) {
+			Request(options, function(error, response, body) {
 				var data = JSON.parse(body);
 				var priceInfo = data.defaultModel.itemPriceResultDO.priceInfo;
 				for (var i in priceInfo) {
